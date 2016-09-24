363f05d2590b090558513421f233608697b33474
toekneeow <toni.au94@gmail.com>
2016-09-24 14:58:46 +0000

    Pushing Ionic 2 Project

---
+++ RUItchyIonic2/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController) {
+  
+  }
+}
